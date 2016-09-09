d6f0bffd75d1c313d42243aa5bc9a1ad1db7932f
Matt Burgess <mattgburgess@gmail.com>
2016-09-09 10:59:53 -0700

    Added extra columns change from earlier to bycatch_cost.

--- bycatch_cost.R
+++ bycatch_cost.R
@@ -23,6 +23,38 @@ bycatch_df <- read_csv("Data/bycatch_species.csv")
 target_df <- read_csv("Data/target_species.csv")
 
 
+##################################################
+##################################################
+### Add pctred fvfmey columns to upsides table ###
+##################################################
+##################################################
+
+ids <- c()
+eqfvfmeys <- c()
+
+# calculate equilibrium MEYs
+for (i in 1:length(upsides$idorig)) {
+  ids <- append(ids, upsides$idorig[i])
+  eqfvfmeys <- append(eqfvfmeys, 
+                      (1/(optim(par = 0.005, fn = function (x) {
+                        - ((upsides$price[i] * upsides$g[i] * x * upsides$k[i] * ((1 - ((upsides$g[i] * x * upsides$phi[i])/(upsides$g[i] * (upsides$phi[i] + 1))))^(1/upsides$phi[i]))) - 
+                             (upsides$marginalcost[i] * ((upsides$g[i] * x)^upsides$beta[i])))
+                      }, method = "Brent", lower = 0, upper = 1.5)$par * (1/upsides$fvfmsy[i])))
+  )
+}
+
+# add equilibrium MEY cols to upsides
+test <- data_frame(idorig = ids, eqfvfmey = eqfvfmeys)
+upsides <- left_join(upsides, test, by = "idorig")
+
+# make pctred columns
+upsides <- upsides %>%
+  mutate(pctredfmsy = 100 * (1 - (1/fvfmsy))) %>%
+  mutate(pctredfmey = 100 * (1 - (1/eqfvfmey))) # defines pctredmey in terms of eqfmey, but we can change this if we want.
+# mutate(pctredfmey = 100 * (1 - (1/(fvfmsy/fmeyvfmsy)))) # defines pctredmey in terms of NPV fmey
+
+
+
 ###################################
 ########## Load functions #########
 ###################################
